ef435bbaa444890432b866911e58aebad3301059
unknown <lightningthunder2k17@gmail.com>
2022-12-13 03:03:07 +0600

    Adding the day 1

---
+++ Day1.py
@@ -0,0 +1,33 @@
+# star='*'
+# x=-1
+# max=6
+# while x<=max :
+#     x=x+1
+#     print(star*max)
+
+# You dont need to google always 
+# print(dir([]))
+# print(help(str.rfind))
+
+# Giving our program the power to think match/case
+while True:
+    dataBase=[]
+    match(input('Please Enter either to show or add')):
+        case 'show' :
+            print(dataBase)
+            break
+        case 'add':
+            tempData=input('what do you want to add')
+            dataBase.append(tempData)
+            print('you can check now ')
+            
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
