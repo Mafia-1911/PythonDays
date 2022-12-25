eef88713d06502e361da086d3a31176d0340ac2a
unknown <lightningthunder2k17@gmail.com>
2022-12-25 17:16:02 +0600

    fstrings

--- Day3.py
+++ Day3.py
@@ -1,3 +1,18 @@
-myList = ['a', 'b','c']
-myList.__setitem__(0,'z')
-print(myList)
+# myList = ['a', 'b','c']             
+# myList.__setitem__(0,'z')             myList[2]='z'
+# print(myList)                         print(myList)
+
+
+# ranking = ['John', 'Sen', 'Lisa']
+# # input the rank number 
+# index= int(input("Enter which position you would wanna see: "))
+# if index<=3:
+#     print(ranking[index-1])
+# else:
+#     print("Sir we didnt rank them ")
+    
+
+# the enumerate function gives you access to both index and items 
+array= [7,4,3,24,5]
+for index,list in enumerate(array):
+    print(f"{index}{list}")
