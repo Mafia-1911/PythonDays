0512df10c06de74ee326cfc0594a6b9fd2c4e327
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-09-16 01:10:04 +0600

    Update Auctions_Project.py

--- PYskillHunt/My Capstone Project Submission/Auctions_Project.py
+++ PYskillHunt/My Capstone Project Submission/Auctions_Project.py
@@ -4,3 +4,8 @@ next="yes";
 while(next=="yes"):
     print(chr(27) + "[2J");                 #It clears the terminal
     name=input("What is your name?");
+ auctioneers["name"].append(name);
+    bid=int(input("What's your bid?"));
+    auctioneers["Bids"].append(bid);
+    print("Please type 'yes' if there are any otherwise type 'no'")
+    next=input("Are there any other bidders?").lower()  #tackling the case-sensitivity
