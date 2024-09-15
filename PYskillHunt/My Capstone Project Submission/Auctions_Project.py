351c9e6dd419bccd7d44e5ea1f08adc1441ecc0c
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-09-16 01:09:48 +0600

    Create Auctions_Project.py

---
+++ PYskillHunt/My Capstone Project Submission/Auctions_Project.py
@@ -0,0 +1,6 @@
+print("Welcome to our Secret Auction")
+auctioneers={"name":[],"Bids":[]}
+next="yes";
+while(next=="yes"):
+    print(chr(27) + "[2J");                 #It clears the terminal
+    name=input("What is your name?");
